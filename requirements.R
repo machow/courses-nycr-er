c2a14100db6d9ca94363de9d249f1718712134de
Michael Chow <chowmichedu@gmail.com>
2018-04-20 11:35:29 -0400

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -2,6 +2,7 @@ library(remotes)
 
 ##### Install specific package versions from CRAN #####
 
+install_version("tidyverse", "1.2.1")
 # install_version("ggplot2", "2.2.1")
 # install_version("dplyr", "0.7.4")
 # install_version("tidyr", "0.8.0")
